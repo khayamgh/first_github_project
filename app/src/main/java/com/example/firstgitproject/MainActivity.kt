e635f947db0b005b8e2563f1175a53fce3100c41
khayamgh <khayamgh2580@gmail.com>
2022-03-19 09:51:42 +0330

    initial commit

---
+++ app/src/main/java/com/example/firstgitproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.firstgitproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        Toast.makeText(this, "hello to all after first commit", Toast.LENGTH_SHORT).show()
+
+
+
+    }
+}
